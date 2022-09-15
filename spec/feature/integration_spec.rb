9afb36dcc29e87481101a60033119fac1d2056f0
Christian Swords <c.swords.9102@tamu.edu>
2022-09-14 21:40:38 -0500

    ci changes 2

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -12,34 +12,6 @@ RSpec.describe 'Creating a book', type: :feature do
 end
 
 
-RSpec.describe 'Creating a book', type: :feature do
-  scenario 'valid inputs' do
-    visit new_book_path
-    fill_in 'Author', with: 'JK Rowling'
-    click_on 'Create Book'
-    visit books_path
-    expect(page).to have_content('JK Rowling')
-  end
-end
 
-RSpec.describe 'Creating a book', type: :feature do
-  scenario 'valid inputs' do
-    visit new_book_path
-    fill_in 'Price', with: '$10'
-    click_on 'Create Book'
-    visit books_path
-    expect(page).to have_content('$10')
-  end
-end
-
-RSpec.describe 'Creating a book', type: :feature do
-  scenario 'valid inputs' do
-    visit new_book_path
-    fill_in 'Date', with: '9/12/2022'
-    click_on 'Create Book'
-    visit books_path
-    expect(page).to have_content('9/12/2022')
-  end
-end
 
 
